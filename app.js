2d17e9f053d20ff5736058edda19685122e50c5e
anovobilskischool <myemail>
2020-12-28 23:56:03 -0800

    new functions and definitions

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ fs.writeFile('index.html',
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <link rel="stylesheet" href="style.css">
-
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
    <title>Document</title>
  </head>
@@ -31,10 +30,107 @@ function (err) {
 });
 }
 
+
+
 start();
+// Function Definitions / Place In Folder For Cleaner File
+function generatemanager(answers) {
+  return `
+
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">${answers.managername}</h1>
+    <p class="lead">My office number is  ${answers.office}.</p>
+    <ul class="list-group">
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+}
+
+function generateengineer(answers) {
+  return `
+
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">${answers.managername}</h1>
+    <p class="lead">My office number is  ${answers.office}.</p>
+    <ul class="list-group">
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+}
 
 // end head write
 
 
 
+var questions = 12345
 
+function oneprompt(){
+inquirer
+  .prompt([
+    {
+      type:'input',
+      message:'Please Input The Manager Name',
+      name:'managername',
+    },
+    {
+      type:'input',
+      message:'Office Number',
+      name:'office',
+    },
+    {
+      type:'input',
+      message:'Enter Employee ID #',
+      name:'managerid',
+    },
+
+    {
+      type:'confirm',
+      message:'Would you like to add another team member?',
+      name:'add',
+    }
+  ])
+  .then(answers => {
+    
+console.log(answers)
+
+if(answers.add === true)
+generatemanager()
+
+
+async function card() {
+ console.log("test")
+ try {
+   const answers = await promptmanager();
+
+   const html = generatemanager(answers);
+
+   await appendfileasync("index.html", html);
+
+   console.log("Successfully appended to index.html");
+ } catch(err) {
+   console.log(err);
+ }
+}
+
+card();if(answers.add === false)
+console.log('awefoij')
+
+
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
+}
+  oneprompt();
