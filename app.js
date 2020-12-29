e801a2da165c9cd1e6802b585dd0bdd34eebacfc
anovobilskischool <myemail>
2020-12-29 14:03:25 -0800

    chad fix

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-
 const writeFileAsync = util.promisify(fs.writeFile);
 const appendfileasync = util.promisify(fs.appendFileSync);
-
 // Function Definitions / Place In Folder For Cleaner File
 
 
-
-  
-
-async function card() {
- console.log("test")
- try {
-   const answers = await promptmanager();
-
-   function generatemanager(answers) {
-    return `
-  
+async function manager(answers) {
+  const html = `
     <div class="jumbotron jumbotron-fluid">
     <div class="container">
       <h1 class="display-4">${answers.managername}</h1>
@@ -30,28 +19,14 @@ async function card() {
   </div>
   </body>
   </html>`;
-  }
-
-   const html = generatemanager(answers);
-
    await appendfileasync("index.html", html);
-
    console.log("Successfully appended to index.html");
- } catch(err) {
-   console.log(err);
- }
 }
 
 
-
-
 // write head //
-
 function start() {
-
 fs.writeFile('index.html', 
-
-
 `
 <!DOCTYPE html>
  <html lang="en">
@@ -65,70 +40,55 @@ fs.writeFile('index.html',
  <body>`,
 function (err) {
   if (err) throw err;
-
 });
 }
 start();
-
-
-
-
-// end head write / begin master prompt
-
-
-
-var questions = 12345
-
 function promptmanager(){
-inquirer
-  .prompt([
-    {
-      type:'input',
-      message:'Please Input The Manager Name',
-      name:'managername',
-    },
-    {
-      type:'input',
-      message:'Office Number',
-      name:'office',
-    },
-    {
-      type:'input',
-      message:'Enter Employee ID #',
-      name:'managerid',
-    },
-
-    {
-      type:'confirm',
-      message:'Would you like to add another team member?',
-      name:'add',
-    }
-  ])
-  .then(answers => {
-
-
+    inquirer
+      .prompt([
+        {
+          message: "What would you like to do?",
+          type: "list",
+          name: "role",
+          choices: ["Add Manager", "Add Engineer", "Add Intern" ]
+      },
     
-console.log(answers)
-
-if(answers.add === true)
-
-
-
-card();
-
-if(answers.add === false)
-console.log('awefoij')
-
-
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
+      {
+        message: "What is the name of the manager",
+        type: "list",
+        name: "namemanager",
+        choices: ["Magnet&Stickers","T-Shirt", "Mug"],
+        when : function( answers ) {
+                return answers.role === "Add Manager"
+                          },
+        default: "T-Shirt"
+    },
+    
+     
+      ])
+      .then(answers => {
+    
+        manager(answers);
+    
+        
+    console.log(answers)
+    
+    if(answers.add === true)
+    
+    
+    
+    
+    if(answers.add === false)
+    console.log('awefoij')
+    
+    
+      })
+      .catch(error => {
+        if(error.isTtyError) {
+          // Prompt couldn't be rendered in the current environment
+        } else {
+          // Something else when wrong
+        }
+      });
     }
-  });
-}
-  promptmanager();
-
-  
+promptmanager();
