bae213a2f1fad04a2ee7aeb032f357cb965759f0
anovobilskischool <myemail>
2020-12-29 14:43:17 -0800

    final

--- app.js
+++ app.js
@@ -1,25 +1,69 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
 const appendfileasync = util.promisify(fs.appendFileSync);
 // Function Definitions / Place In Folder For Cleaner File
 
 
 async function manager(answers) {
-  const html = `
+  const htmlmanager = `
     <div class="jumbotron jumbotron-fluid">
     <div class="container">
-      <h1 class="display-4">${answers.managername}</h1>
-      <p class="lead">My office number is  ${answers.office}.</p>
+    <h1><b>Manager</b></h1>
+      <h2 class="display-4">${answers.managername}</h2>
+      <p class="lead">My office number is  ${answers.manageroffice}.</p>
       <ul class="list-group">
-        <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+        <li class="list-group-item">ID#: ${answers.managerid}</li>
+        <li class="list-group-item">Email Address: ${answers.manageremail}</li>
+
       </ul>
     </div>
   </div>
   </body>
   </html>`;
-   await appendfileasync("index.html", html);
+   await appendfileasync("index.html", htmlmanager);
+   console.log("Successfully appended to index.html");
+}
+
+async function engineer(answers) {
+  const htmlengineer = 
+  `
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+  <h1><b>Engineer</b></h1>
+    <h2 class="display-4">${answers.engineername}</h2>
+    <ul class="list-group">
+      <li class="list-group-item">ID#: ${answers.engineerid}</li>
+      <li class="list-group-item">Email Address: ${answers.engineeremail}</li>
+      <li class="list-group-item">Github: ${answers.engineergithub}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>
+`;
+
+   await appendfileasync("index.html", htmlengineer);
+   console.log("Successfully appended to index.html");
+}
+
+async function intern(answers) {
+  const internhtml =   `
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+  <h1><b>Intern</b></h1>
+    <h2 class="display-4">${answers.internname}</h2>
+    <ul class="list-group">
+      <li class="list-group-item">ID#: ${answers.internid}</li>
+      <li class="list-group-item">Email Address: ${answers.internemail}</li>
+      <li class="list-group-item">School: ${answers.internschool}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>
+`;
+   await appendfileasync("index.html", internhtml);
    console.log("Successfully appended to index.html");
 }
 
@@ -43,7 +87,10 @@ function (err) {
 });
 }
 start();
-function promptmanager(){
+
+// Prompt Function Declarations //
+
+function promptstart(){
     inquirer
       .prompt([
         {
@@ -51,38 +98,26 @@ function promptmanager(){
           type: "list",
           name: "role",
           choices: ["Add Manager", "Add Engineer", "Add Intern" ]
-      },
-    
-      {
-        message: "What is the name of the manager",
-        type: "list",
-        name: "namemanager",
-        choices: ["Magnet&Stickers","T-Shirt", "Mug"],
-        when : function( answers ) {
-                return answers.role === "Add Manager"
-                          },
-        default: "T-Shirt"
-    },
-    
-     
-      ])
+      },])
+
       .then(answers => {
     
-        manager(answers);
     
-        
-    console.log(answers)
+        if (answers.role === "Add Manager"){
+promptmanager()
+        }
+
+  else if  (answers.role === "Add Engineer"){
+  promptengineer()
+  }
+  else if (answers.role === "Add Intern"){
+    promptintern()
+        }
+
     
-    if(answers.add === true)
-    
-    
-    
-    
-    if(answers.add === false)
-    console.log('awefoij')
-    
-    
+      
       })
+
       .catch(error => {
         if(error.isTtyError) {
           // Prompt couldn't be rendered in the current environment
@@ -91,4 +126,171 @@ function promptmanager(){
         }
       });
     }
-promptmanager();
+
+    function promptmanager(){
+      inquirer
+        .prompt([
+          {
+            message: "What is the managers name?",
+            type: "input",
+            name: "managername",
+        },
+        {
+          message: "What is the managers employee id number?",
+          type: "number",
+          name: "managerid",
+      },
+      {
+        message: "What is the managers office number?",
+        type: "number",
+        name: "manageroffice",
+    },
+    {
+      message: "What is the managers email address?",
+      type: "input",
+      name: "manageremail",
+  },
+      
+      
+      ])
+  
+        .then(answers => {
+      
+      manager(answers),addmore()
+
+  
+      
+      
+        })
+        .catch(error => {
+          if(error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+          } else {
+            // Something else when wrong
+          }
+        });
+      }
+
+      function promptengineer(){
+        inquirer
+          .prompt([
+            {
+              message: "What is the engineers name?",
+              type: "input",
+              name: "engineername",
+          },
+          {
+            message: "What is the engineers employee id number?",
+            type: "number",
+            name: "engineerid",
+        },
+        {
+          message: "What is the engineers email addresss?",
+          type: "input",
+          name: "engineeremail",
+      },
+      {
+        message: "What is the engineers github?",
+        type: "input",
+        name: "engineergithub",
+    },
+        
+        
+        ])
+    
+          .then(answers => {
+        
+        engineer(answers),addmore()
+  
+    
+        
+        
+          })
+          .catch(error => {
+            if(error.isTtyError) {
+              // Prompt couldn't be rendered in the current environment
+            } else {
+              // Something else when wrong
+            }
+          });
+        }
+        function promptintern(){
+          inquirer
+            .prompt([
+              {
+                message: "What is the interns name?",
+                type: "input",
+                name: "internname",
+            },
+            {
+              message: "What is the interns employee id number?",
+              type: "number",
+              name: "internid",
+          },
+          {
+            message: "What is the interns email addresss?",
+            type: "input",
+            name: "internemail",
+        },
+        {
+          message: "What is the interns school?",
+          type: "input",
+          name: "internschool",
+      },
+          
+          
+          ])
+      
+            .then(answers => {
+          
+          intern(answers),addmore()
+    
+      
+          
+          
+            })
+            .catch(error => {
+              if(error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+              } else {
+                // Something else when wrong
+              }
+            });
+          }
+
+
+      function addmore(){
+        inquirer
+          .prompt([
+            {
+              message: "Would you like to add another employee?",
+              name:'addmore',
+              type: "confirm",
+          },
+         
+        
+        
+        ])
+    
+          .then(answers => {
+        
+if (answers.addmore === true)
+promptstart()
+
+else if (answers.addmore === false)
+console.log('done! check out index.html to see your team')    
+        
+        
+          })
+          .catch(error => {
+            if(error.isTtyError) {
+              // Prompt couldn't be rendered in the current environment
+            } else {
+              // Something else when wrong
+            }
+          });
+        }
+
+
+
+promptstart();
